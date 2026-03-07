e0d2a2ca9c439374b6dd930778d51c4cb4f92fe3
gom Alaska <sya112567@gmail.com>
2026-03-07 23:59:40 +0900

    1차확인

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(67, 64);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(231, 209);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.White;
+            label1.Font = new Font("맑은 고딕", 36F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.Location = new Point(345, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(215, 81);
+            label1.TabIndex = 1;
+            label1.Text = "김민욱";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("새굴림", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label2.ForeColor = Color.Blue;
+            label2.Location = new Point(368, 172);
+            label2.Name = "label2";
+            label2.Size = new Size(144, 28);
+            label2.TabIndex = 2;
+            label2.Text = "컴퓨터SW";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.White;
+            label3.Font = new Font("맑은 고딕", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label3.ForeColor = Color.Fuchsia;
+            label3.Location = new Point(368, 223);
+            label3.Name = "label3";
+            label3.Size = new Size(403, 50);
+            label3.TabIndex = 3;
+            label3.Text = "sya112567@gmail.com";
+            label3.Click += label3_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 192, 192);
+            button1.Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button1.Location = new Point(529, 335);
+            button1.Name = "button1";
+            button1.Size = new Size(199, 66);
+            button1.TabIndex = 4;
+            button1.Text = "배경색 랜덤 변경";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Lime;
+            button2.Font = new Font("맑은 고딕", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button2.Location = new Point(381, 347);
+            button2.Name = "button2";
+            button2.Size = new Size(142, 39);
+            button2.TabIndex = 5;
+            button2.Text = "GitHub";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(192, 255, 255);
+            button3.Font = new Font("휴먼둥근헤드라인", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button3.Location = new Point(103, 290);
+            button3.Name = "button3";
+            button3.Size = new Size(168, 69);
+            button3.TabIndex = 6;
+            button3.Text = "사진 변경";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
             ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,37 @@ namespace MyBusinessCard
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            // 0~255 사이의R, G, B 값을무작위로생성하여배경색에적용
+            this.BackColor = Color.FromArgb(rd.Next(256), rd.Next(256), rd.Next(256));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/alaskagom");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.images;
+        }
     }
 }
