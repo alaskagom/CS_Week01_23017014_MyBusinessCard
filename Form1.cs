e0f7dbd8d01c0958c32b8fd226b85d841b17e201
gom Alaska <sya112567@gmail.com>
2026-03-08 00:35:53 +0900

    2

--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,18 @@ namespace MyBusinessCard
 {
     public partial class Form1 : Form
     {
+        private System.Drawing.Image originalImage;
+        private bool isAlternateImage = false;
+
         public Form1()
         {
             InitializeComponent();
+            // 초기 표시된 이미지를 저장하여 토글 시 복원할 수 있게 함
+            originalImage = pictureBox1.Image;
+
+            // 마우스 효과 이벤트 연결
+            pictureBox1.MouseEnter += pictureBox1_MouseEnter;
+            pictureBox1.MouseLeave += pictureBox1_MouseLeave;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -31,12 +40,39 @@ namespace MyBusinessCard
 
         private void button2_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start("https://github.com/alaskagom");
+            var psi = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://github.com/alaskagom",
+                UseShellExecute = true
+            };
+            System.Diagnostics.Process.Start(psi);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            pictureBox1.Image = Properties.Resources.images;
+            // 사진을 토글: 현재가 기본이면 리소스 이미지로 바꾸고, 아니면 원래 이미지로 복원
+            if (!isAlternateImage)
+            {
+                pictureBox1.Image = Properties.Resources.images;
+            }
+            else
+            {
+                pictureBox1.Image = originalImage;
+            }
+
+            isAlternateImage = !isAlternateImage;
+        }
+
+        // 마우스가 사진 위로 올라갈 때 테두리 스타일 적용
+        private void pictureBox1_MouseEnter(object? sender, EventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        // 마우스가 사진에서 벗어날 때 원래 테두리(없음)로 복원
+        private void pictureBox1_MouseLeave(object? sender, EventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.None;
         }
     }
 }
